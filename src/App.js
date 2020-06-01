360ba68864b8a7781e812dfa1e101374529354bc
kirperov <kirill.petrov.centrale@gmail.com>
2020-06-01 14:27:13 +0200

    Init markdown project, import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { sampleText } from './sampleText'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { render } from '@testing-library/react';
+
+class App extends Component {
+  state = {
+    text: sampleText 
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea className="form-control"
+                      onChange={ this.handleChange }
+                      rows="35"
+                      value={ this.state.text }>
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+          { sampleText }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
